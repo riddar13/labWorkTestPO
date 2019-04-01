19177d795062133444b2574d565f7fcc2ee04eab
riddar13 <40752707+riddar13@users.noreply.github.com>
2019-04-01 13:17:06 +0600

    tests for eighth is complete

--- LabPO/LabPO.Tests/EighthTaskTests.cs
+++ LabPO/LabPO.Tests/EighthTaskTests.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LabPO.Tests
 {
-    class EighthTaskTests
+    [TestClass]
+    public class EighthTaskTests
     {
+        [TestMethod]
+        public void FirstTest()
+        {
+            int[,] x = { 
+                { -5, 1}, 
+                { -2, 9} };
+            int[,] expected = { 
+                {-5, -6, 1}, 
+                {-2, -11, 9} };
+            EighthTask c = new EighthTask();
+            var actual = c.Method(x);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void SecondTest()
+        {
+            int[,] x = {
+                { -5, 9},
+                { -2, 1} };
+            int[,] expected = {
+                {-5, -14, 9},
+                {-2, -3, 1} };
+            EighthTask c = new EighthTask();
+            var actual = c.Method(x);
+            CollectionAssert.AreEqual(expected, actual);
+        }
     }
 }
