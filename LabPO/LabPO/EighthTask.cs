803ab57d91f917ed02e4c4362e0db4cd9a737192
riddar13 <40752707+riddar13@users.noreply.github.com>
2019-04-01 13:02:14 +0600

    add comments

--- LabPO/LabPO/EighthTask.cs
+++ LabPO/LabPO/EighthTask.cs
@@ -10,6 +10,7 @@ namespace LabPO
     {
         public int[,] Method(int[,] a)
         {
+            // находим минимальный элемент и сохраняем его коорды
             int minElement = a[0, 0] , minX = 0, minY = 0;
             for (int i = 0; i < a.GetLength(0); i++)
             {
@@ -23,7 +24,7 @@ namespace LabPO
                     }
                 }
             }
-
+            // находим максимальный элемент и сохраняем его коорды
             int maxElement = a[0, 0], maxX = 0, maxY = 0;
             for (int i = 0; i < a.GetLength(0); i++)
             {
@@ -37,7 +38,8 @@ namespace LabPO
                     }
                 }
             }
-
+            // создаем новый массив в котором сохраняем разнизцу: 
+            // минСтолбец - максСтолбец
             int[,] stolb = new int[a.GetLength(0), 1];
             for (int i = 0; i < a.GetLength(0); i++)
             {
@@ -45,6 +47,9 @@ namespace LabPO
                 Console.WriteLine(stolb[i, 0]);
             }
 
+            // считаем колво столбцов, в которых есть четный отрицательный
+            // элемент и сохраняем в новом массиве 1 или 0
+            // 1 - есть отрицательный, 0 - нет отрицательного
             int colvoOtric = 0, colvo = 0;
             int[] chetOtric = new int[a.GetLength(1)];
             for (int j = 0; j < a.GetLength(1); j++)
@@ -64,11 +69,17 @@ namespace LabPO
                 colvo = 0;
             }
             Console.WriteLine();
+            // это я для проверки правильности выполнения кода выводил
            /* foreach(int arrr in chetOtric)
             {
                 Console.WriteLine(chetOtric[arrr]);
             }*/
 
+            // создали новую матрицу с увеличенным колвом столбцов
+            // и теперь тупо переписываем значения в новую матрицу
+            // если в элементе массива, который хранит 0 и 1, единица, то записываем в массив 
+            // столбец из начальной матрицы и столбец разницы
+            // иначе только столбец из начальной матрицы
             int[,] resultMatrix = new int[a.GetLength(0),a.GetLength(1) + colvoOtric];
             int schet = 0;
             for (int j = 0; j < resultMatrix.GetLength(1); j++)
